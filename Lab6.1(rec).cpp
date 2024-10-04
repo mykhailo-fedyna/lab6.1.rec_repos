b729576bf7bb538e316653a3bc2d6a4766712062
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-04 16:55:12 +0300

    Changed code

--- Lab6.1(rec).cpp
+++ Lab6.1(rec).cpp
@@ -1,55 +1,88 @@
-﻿// lab6.1(rec).cpp
+﻿//// lab6.1(rec).cpp
 #include <iostream>
 #include <iomanip>
 #include <time.h>
 using namespace std;
+
 void Create(int* r, const int size, const int Low, const int High, int i)
 {
-	r[i] = Low + rand() % (High - Low + 1);
-	if (i < size - 1)
-		Create(r, size, Low, High, i + 1);
+    r[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(r, size, Low, High, i + 1);
 }
+
 void Print(int* r, const int size, int i)
 {
-	cout << setw(4) << r[i];
-	if (i < size - 1)
-		Print(r, size, i + 1);
-	else
-		cout << endl;
+    cout << setw(4) << r[i];
+    if (i < size - 1)
+        Print(r, size, i + 1);
+    else
+        cout << endl;
 }
-int Sum(int* r, const int size, int i, int S, int& count_element)
+
+int CountElements(int* r, const int size, int i, int& count_element)
 {
-	if (i < size)
-	{
-		if (r[i] % 6 == 0 || !(i % 5 == 0))
-		{
-			count_element++;
-			int current_value = r[i];
-			r[i] = 0;
-			return current_value + Sum(r, size, i + 1, S,  count_element);
-		}
-		else
-		{
-			return Sum(r, size, i + 1, S, count_element);
-		}
-	}
-	else
-		return 0;
+    if (i < size)
+    {
+        if (r[i] % 6 == 0 || i % 5 != 0)
+        {
+            count_element++;
+        }
+        return CountElements(r, size, i + 1, count_element);
+    }
+    return count_element;
 }
+
+int SumElements(int* r, const int size, int i)
+{
+    if (i < size)
+    {
+        if (r[i] % 6 == 0 || i % 5 != 0)
+        {
+            return r[i] + SumElements(r, size, i + 1);
+        }
+        else
+        {
+            return SumElements(r, size, i + 1);
+        }
+    }
+    return 0;
+}
+
+void ReplaceElements(int* r, const int size, int i)
+{
+    if (i < size)
+    {
+        if (r[i] % 6 == 0 || i % 5 != 0)
+        {
+            r[i] = 0;
+        }
+        ReplaceElements(r, size, i + 1);
+    }
+}
+
 int main()
 {
-	srand((unsigned)time(NULL));
-	const int n = 25;
-	int r[n];
-	int Low = 4;
-	int High = 73;
-	int count_element = 0;
-	Create(r, n, Low, High, 0);
-	Print(r, n, 0);
+    srand((unsigned)time(NULL));
+    const int n = 25;
+    int r[n];
+    int Low = 4;
+    int High = 73;
+    int count_element = 0;
 
-	int sum = Sum(r, n, 0, 0, count_element);
-	cout << "S = " << sum << endl;
-	cout << "Count of element = " << count_element << endl;
-	Print(r, n, 0);
-	return 0;
+    Create(r, n, Low, High, 0);
+
+    Print(r, n, 0);
+
+    int sum = SumElements(r, n, 0);
+    cout << "S = " << sum << endl;
+
+    CountElements(r, n, 0, count_element);
+    cout << "Count of elements = " << count_element << endl;
+
+    ReplaceElements(r, n, 0);
+
+    Print(r, n, 0);
+
+    return 0;
 }
--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -9,19 +9,37 @@ namespace UnitTest1
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestSumElements)
 		{
 			const int n = 5;
 			int r[n] = { 12, 5, 6, 15, 18 };
-			int count_element = 0;
-			int expected_sum = 56;
-			int expected_count = 5;
 
-			int actual_sum = Sum(r, n, 0, 0, count_element);
+			int expected_sum = 56;
+
+			int actual_sum = SumElements(r, n, 0);
 
 			Assert::AreEqual(expected_sum, actual_sum);
+		}
+
+		TEST_METHOD(TestCountElements)
+		{
+			const int n = 5;
+			int r[n] = { 12, 5, 6, 15, 18 };
+
+			int expected_count = 5;
+
+			int count_element = 0;
+			CountElements(r, n, 0, count_element);
 
 			Assert::AreEqual(expected_count, count_element);
+		}
+
+		TEST_METHOD(TestReplaceElements)
+		{
+			const int n = 5;
+			int r[n] = { 12, 5, 6, 15, 18 };
+
+			ReplaceElements(r, n, 0);
 
 			Assert::AreEqual(0, r[0]);
 			Assert::AreEqual(0, r[1]);
